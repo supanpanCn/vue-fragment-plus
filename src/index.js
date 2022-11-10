d3966cc0864a5d83da6676a154b0f5194b1a7a95
{苏盼盼} <1372978934@qq.comqq.com>
2022-11-10 21:57:13 +0800

    缺少出口

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import { precessDomApi } from './utils'
 import component from './component.js'
-export const frag = {
+export const Fragment = {
   install(Vue){
     precessDomApi()
     Vue.component('Fragment', component)
   }
+}
+export default {
+  Fragment
 }
